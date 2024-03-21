a06a1cc4fb7292116b4eaab68dd57b2be30e7d97
PM2.6 <bijibao@123.com>
2024-03-21 17:13:19 +0800

    订单数据统计实现

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -3,6 +3,7 @@ package com.sky.controller.admin;
 
 import com.sky.result.Result;
 import com.sky.service.ReportService;
+import com.sky.vo.OrderReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
@@ -59,5 +60,23 @@ public class ReportController {
         log.info("用户数据统计 ：{}，{}",begin,end);
         UserReportVO userReportVO=reportService.getUserStatistics(begin,end);
         return Result.success(userReportVO);
+    }
+
+
+
+
+    @GetMapping("/ordersStatistics")
+    @ApiOperation("订单统计")
+    public Result<OrderReportVO> OrdersStatistics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd")
+                    LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd")
+                    LocalDate end
+    )
+    {
+
+        log.info("用户数据统计 ：{}，{}",begin,end);
+       OrderReportVO orderReportVO =reportService.getOrderStatistics(begin,end);
+        return Result.success(orderReportVO);
     }
 }
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -4,6 +4,7 @@ import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
+import com.sky.vo.OrderReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
@@ -125,5 +126,74 @@ public class ReportServiceImpl implements ReportService {
                 newUserList(StringUtils.join(newUserList,","))
                 .totalUserList(StringUtils.join(totalUserList,","))
                 .build();
+    }
+
+    /**
+     * 统计各时间段的订单数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    public OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end) {
+        List<LocalDate> dateList=new ArrayList<>();//每天日期
+
+        dateList.add(begin);
+        while (!begin.equals(end))
+        {
+
+            begin=begin.plusDays(1);
+            dateList.add(begin);
+        }
+
+        List<Integer> orderCountList=new ArrayList<>();
+        List<Integer> validOrderCountList=new ArrayList<>();
+        Integer totalOrderCount=0;
+        Integer validtotalOrderCount=0;
+        for (LocalDate localDate : dateList) {
+
+            LocalDateTime begintime = LocalDateTime.of(localDate, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(localDate, LocalTime.MAX);
+
+
+            //每日订单总数
+
+            Integer orderCount = getOrderCount(begintime, endTime,null);
+            totalOrderCount+=orderCount;
+            orderCountList.add(orderCount);
+
+
+            //每日有效订单数
+            Integer validOrderCount = getOrderCount(begintime, endTime, Orders.COMPLETED);
+            validtotalOrderCount+=validOrderCount;
+            validOrderCountList.add(validOrderCount);
+
+        }
+        //订单完成率
+        Double orderCompletionRate=0.0;
+        if (totalOrderCount !=0)
+        {orderCompletionRate=validtotalOrderCount.doubleValue()/totalOrderCount;}
+
+        return OrderReportVO.builder()
+                .dateList(StringUtils.join(dateList,","))
+                .orderCountList(StringUtils.join(orderCountList,","))
+                .validOrderCountList(StringUtils.join(validOrderCountList,","))
+                .totalOrderCount(totalOrderCount)
+                .validOrderCount(validtotalOrderCount)
+                .orderCompletionRate(orderCompletionRate)
+                .build();
+    }
+
+    //查询订单数量
+    private Integer getOrderCount(LocalDateTime begin,LocalDateTime end,Integer status)
+    {
+
+        Map map=new HashMap();
+        map.put("begin",begin);
+        map.put("end",end);
+        map.put("status",status);
+
+       return orderMapper.countByMap(map);
+
+
     }
 }
