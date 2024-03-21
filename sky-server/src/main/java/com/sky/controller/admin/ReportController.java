5fd1dc875b541b57b56c8e2dd0153b1f9807732f
PM2.6 <bijibao@123.com>
2024-03-21 16:32:18 +0800

    用户数据统计

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -4,6 +4,7 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -40,5 +41,23 @@ public class ReportController {
         TurnoverReportVO turnoverStatistics = reportService.getTurnoverStatistics(begin, end);
         return Result.success(turnoverStatistics);
 
+    }
+
+
+
+
+    @GetMapping("/userStatistics")
+    @ApiOperation("用户统计")
+    public Result<UserReportVO> userStatistics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd")
+                    LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd")
+                    LocalDate end
+    )
+    {
+
+        log.info("用户数据统计 ：{}，{}",begin,end);
+        UserReportVO userReportVO=reportService.getUserStatistics(begin,end);
+        return Result.success(userReportVO);
     }
 }
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,8 +2,10 @@ package com.sky.service.impl;
 
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,10 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     private OrderMapper orderMapper;
+
+
+    @Autowired
+    private UserMapper userMapper;
 
     /**
      * 营业额统计
@@ -68,5 +74,56 @@ public class ReportServiceImpl implements ReportService {
        return TurnoverReportVO.builder().dateList(dateString).turnoverList(turnoverString).build();
 
 
+    }
+
+
+    /**
+     * 用户统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+
+        List<LocalDate> dateList=new ArrayList<>();//每天日期
+
+        dateList.add(begin);
+        while (!begin.equals(end))
+        {
+
+            begin=begin.plusDays(1);
+            dateList.add(begin);
+        }
+
+        List<Integer> newUserList=new ArrayList<>();
+        List<Integer> totalUserList=new ArrayList<>();
+        for(LocalDate localDate:dateList)
+        {
+            LocalDateTime begintime = LocalDateTime.of(localDate, LocalTime.MIN);
+            LocalDateTime endtime = LocalDateTime.of(localDate, LocalTime.MAX);
+
+            Map map=new HashMap();
+            map.put("end",endtime);
+            Integer totaluser = userMapper.countByMap(map);
+            totalUserList.add(totaluser);
+
+            map.put("begin",begintime);
+            Integer newuser=userMapper.countByMap(map);
+
+            newUserList.add(newuser);
+        }
+
+
+
+
+
+
+
+        return UserReportVO
+                .builder()
+                .dateList(StringUtils.join(dateList,",")).
+                newUserList(StringUtils.join(newUserList,","))
+                .totalUserList(StringUtils.join(totalUserList,","))
+                .build();
     }
 }
